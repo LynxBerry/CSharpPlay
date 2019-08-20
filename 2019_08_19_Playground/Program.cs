9a1c669307d3aa1ab13b7e2577bbaf7a5b4e0e64
Steven Shao <stevensh@microsoft.com>
2019-08-20 14:38:31 +0800

    init playground

---
+++ 2019_08_19_Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _2019_08_19_Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = {2, 3, 4, 5};
+            IEnumerable<int> query = testArray.Take(2);
+            int res = Array.BinarySearch(testArray,3);
+            query.Select(x=>"xx").ToList().ForEach(x=>Console.WriteLine($"{x}"));
+            
+            bool test = testArray.Any();
+
+            Console.Write("");
+        }
+    }
+}
